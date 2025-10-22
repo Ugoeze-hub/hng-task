4a1cfb407f7315bc30257dc396771ca71043eec7
Ugoeze-hub <eluchieugoeze@gmail.com>
2025-10-22 20:14:36 +0100

    ready to deploy 6

--- string_analyzer/urls.py
+++ string_analyzer/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('strings/', strings, name='strings'),
+    path('strings', strings, name='strings'),
     path('strings/filter-by-natural-language/', natural_language_filter, name='natural_language_filter'),
     path('strings/<str:specific_string>/', get_remove_string, name='get_remove_string'),
 ]
